5fc9fafb45f4f78c2f3e7c0e9f2eb346f7cf5bff
Prateek938 <163449051+Prateek938@users.noreply.github.com>
2024-03-26 17:43:36 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('new.html')
+
+@app.route('/prof')
+def prof():
+    return render_template('prof.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
